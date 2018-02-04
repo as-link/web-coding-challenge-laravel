0aa93856419fc861f4d0198eae033e161688436d
Ed Sebti <sebti.1ahmed@gmail.com>
2018-02-04 09:59:08 +0000

    Fix cors error
    Fix “Class cors does not exist” error

--- config/app.php
+++ config/app.php
@@ -167,7 +167,8 @@ return [
         /*
          * Package Service Providers...
          */
-		Laravel\Passport\PassportServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
